8ac02bb13d4d310776cb6dc248e1d607b1d94c48
StephenRoche1 <92515027+StephenRoche1@users.noreply.github.com>
2022-03-14 06:39:04 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from methodFile import DevTools as DT
 #Devtools  is a class file created for the purposes of pulling json data
 #a simple snippet of the base tools i have created over the years
 active=True
-citySearch= input("input the city you would like to search")
-myCitylat = DT.COOr(citySearch)
-lattitude=myCitylat['latt'][0]#use the first indexed iteration of longitude and latitude as it
-longitude = myCitylat['longt'][0]
-DT.temps(lattitude,longitude) #apply the plot using the pandas function
+while active == True:
+    citySearch= input("input the city you would like to search")
+    myCitylat = DT.COOr(citySearch)
+    lattitude=myCitylat['latt'][0]#use the first indexed iteration of longitude and latitude as it
+    longitude = myCitylat['longt'][0]
+    DT.temps(lattitude,longitude) #apply the plot using the pandas function
+    continues=input('would you like to search again press any key to search again or press q to quit')
+    if continues == 'q':
+        active = False
+
 
 
 
